67956664b0dcedb26f685c38b1b457fc60cea135
andr3w <liu.anray+github@gmail.com>
2018-02-17 22:46:17 -0500

    Add button and password field

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { ipcRenderer } from 'electron';
 import Reboot from 'material-ui/Reboot';
+import TextField from 'material-ui/TextField';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
 
-export default class App extends React.Component {
+const styles = theme => ({
+  container: {
+    marginTop: theme.spacing.unit * 8,
+  },
+});
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,13 +25,34 @@ export default class App extends React.Component {
     ipcRenderer.on('files-selected', (event, selectedFiles) => {
       console.log(selectedFiles);
     });
-    ipcRenderer.send('show-open-dialog');
+    // ipcRenderer.send('show-open-dialog');
   }
 
   render() {
-    return (<div>
-      <Reboot />
-      <h2>Welcome to React!</h2>
-    </div>);
+    const { classes } = this.props;
+    return (
+      <Grid container direction="column" alignItems="center" className={classes.container}>
+        <Reboot/>
+        <Grid item>
+          <Typography variant="title">Welcome to ZipCryptor!</Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="raised">Select files</Button>
+        </Grid>
+        <Grid item>
+          
+        <Grid item>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+          />
+        </Grid>
+      </Grid>
+    );
   }
 }
+
+export default withStyles(styles)(App);
